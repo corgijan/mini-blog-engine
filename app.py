093dc93296b786e4f3b6f105a2c2824301c2b6b6
Iamjava <jan.vaorin@tu-dortmund.de>
2023-08-22 09:49:21 +0200

    edit

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ header = """
                 }
                 </style>
                 <main>
+                <a href="/">HOME</a> <a href="/e/new">HINZUFÜGEN</a>
                 """
 footer = """
                 </main>
@@ -53,30 +54,33 @@ main_page = """
                 REZEPTE: 
                     <ul>
                 {% for r in recepies%}
-                    <li><h2><a href="/r/{{r.hash}}">{{ r.title }}</a></h2></li>
+                    <li><h2><a href="/r/{{r.id}}">{{ r.title }}</a></h2></li>
                 {% endfor %}
                 </ul>
-                <details>
-                <summary>Rezept hinzufügen</summary>
-                <form method="post" class="indent">
-                Titel :<br> <input name="title" /><br>
-                Tags (Kommaseparierte Liste):<br> <input name="tags" /><br>
-                Zutaten:<br> <textarea name="ingredients" rows="5" cols="33"></textarea><br>
-                Rezept:<br> <textarea name="prep" rows="5" cols="33"></textarea><br>
-                <br>
-                Passphrase:<br> <input name="pass" /><br>
-                <button type="submit">Abschicken</button>
-                </form>
-                </details>
                 """
 
+edit_page= """
+                <form method="post" action="/" class="indent">
+                Titel :<br> <input name="title" value="{{r.title}}"></input><br>
+                Tags (Kommaseparierte Liste):<br> <input name="tags" value="{{r.tags}}"/><br>
+                Zutaten:<br> <textarea name="ingredients" rows="5" cols="33">{{r.ingredients}}</textarea><br>
+                Zubereitung:<br> <textarea name="prep" rows="5" cols="33"> {{r.prep}}</textarea><br>
+                <br>
+                Passphrase:<br> <input name="pass"/><br>
+                <input type="hidden" name="id" value="{{r.id}}"/>
+                <button type="submit">Abschicken</button>
+                </form>
+
+"""
+
 recepie_page = """
-                <p>Hello at Bäckerone, your responsible disclosure service for recepies!</p>
+                    <p>Hello at Bäckerone, your responsible disclosure service for recepies!</p>
                     <h1>{{r.title}}</h1><br>
                     <h3> Zutaten: </h3>
-                    <h4>{{r.ingredients}}</h4><br>
+                    <h4><pre>{{r.ingredients}}</pre></h4><br>
                     <h3> Zubereitung: </h3>
-                    <h4>{{r.prep}}<h4>
+                    <h4><pre>{{r.prep}}</pre><h4>
+                    <a href="/e/{{r.id}}">Editieren</a>
                 """
 
 
@@ -90,28 +94,50 @@ def main():
             rezepte = []
         if request.method == "POST":
             if request.form["pass"]=="ichessegernekuchen":
-                rezept = dict(title=request.form["title"][0:3000],ingredients=request.form["ingredients"][0:3000],prep=request.form["prep"][0:3000],tags=request.form["tags"][0:3000] ,hash=uuid.uuid4().__str__())
+                rezept = dict(title=request.form["title"][0:3000],ingredients=request.form["ingredients"][0:3000],prep=request.form["prep"][0:3000],tags=request.form["tags"][0:3000] ,id=uuid.uuid4().__str__())
+                print(request.form["title"])
+                if request.form.get("id")!=None:
+                    rezepte = list(filter(lambda x: x["id"] != request.form["id"],rezepte))
                 rezepte.append(rezept)
+                print(rezept)
                 with open("data.txt","w") as data:
                     data.write(json.dumps(rezepte,indent=2))
-                return redirect("/r/"+rezept["hash"])
+                return redirect("/r/"+rezept["id"])
             else:
-                return page("FALSCHE PASSPHRASE, Rezept nicht gefunden")
+                return page("FALSCHE PASSPHRASE, Rezept nicht angelegt / editiert")
         environment = jinja2.Environment()
         template = environment.from_string(page(main_page))
         return template.render(recepies=rezepte)
            
 
-
-@app.route("/r/<id>")
-def rezepte(id):
+@app.route("/e/<id>")
+def rezepte_edit(id):
     rezepte = []
     with open("data.txt","r") as data:
         try: 
             rezepte: List[dict] = json.loads(data.read())
         except:
             rezepte = []
-        rezept = list(filter(lambda x: x["hash"] == id,rezepte))
+        if id!="new":
+            rezept = list(filter(lambda x: x["id"] == id,rezepte))
+            if rezept==[]: 
+                return page("Rezept nicht gefunden :(")
+            rezept = rezept[0]
+        else:
+            rezept = dict(title="",tags="",prep="",ingredients="",id="")
+        environment = jinja2.Environment()
+        template = environment.from_string(page(edit_page))
+        return template.render(r=rezept)
+
+@app.route("/r/<id>")
+def rezepte_show(id):
+    rezepte = []
+    with open("data.txt","r") as data:
+        try: 
+            rezepte: List[dict] = json.loads(data.read())
+        except:
+            rezepte = []
+        rezept = list(filter(lambda x: x["id"] == id,rezepte))
         if rezept!=[]:
             rezept = rezept[0]
             environment = jinja2.Environment()
