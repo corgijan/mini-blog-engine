585e58e14ba41c6ec263be4e81f19e6f9791d536
Iamjava <jan.vaorin@tu-dortmund.de>
2023-09-02 09:30:49 +0200

    touchup

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ import jinja2, uuid, os, sqlite3, json
 app = Flask(__name__)
 app.secret_key = os.urandom(64)
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
-DB_DRIVER = "SQLITE" # JSON or SQLITE
+
+DB_DRIVER = "JSON"  # JSON or SQLITE
+DATAFILE = "data.json"
+PASSPHRASE = os.environ.get("RECIPE_PASSPHRASE") or "ichessegernekuchen"
 
 header = """
                 <!DOCTYPE html>
@@ -26,6 +29,7 @@ header = """
                     border: 3px solid #264653;
                 }
                 main { padding: 40px; max-width: 800px; }
+                img {max-width:300px;max-height:300px;}
                 details{
                     padding:40px 0 40px 0;
                     background: #2A9D8F;
@@ -105,49 +109,62 @@ recipe_page = """
                 <a href="/e/{{r.id}}">Editieren</a>
                 """
 
+
 def page(name):
     return header + name + footer
+
 
 def get_sqlite_db():
     if 'db' not in g:
         g.db = sqlite3.connect('data.db')
         g.db.row_factory = sqlite3.Row
-        g.db.execute("CREATE TABLE IF NOT EXISTS recipes (id text PRIMARY KEY, title text NOT NULL, ingredients text, prep text, tags text, cvss real)")
+        g.db.execute(
+            "CREATE TABLE IF NOT EXISTS recipes (id text PRIMARY KEY, title text NOT NULL, ingredients text, prep text, tags text, cvss real)")
     return g.db
+
 
 def get_json_db():
     try:
-        with open('data.json') as db_file:
+        with open(DATAFILE) as db_file:
             db_recipes = json.load(db_file)
     except Exception:
         db_recipes = {}
     return [{"id": id, **recipe} for id, recipe in db_recipes.items()]
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
     db = g.pop('db', None)
     if db is not None: db.close()
 
+
 @app.route("/", methods=["GET", "POST"])
 def main():
     if request.method == "POST":
-        if request.form["pass"] == (os.environ.get("RECIPE_PASSPHRASE") or "ichessegernekuchen") or 'authenticated' in session:
+        if request.form["pass"] == PASSPHRASE or 'authenticated' in session:
             session['authenticated'] = True
             if request.form["title"] == "": return page("Bitte wenigstens einen Titel eingeben")
-            if request.form["del-title"] != "" and request.form["del-title"] != request.form["title"]: return page("TITEL NICHT KORREKT, Rezept wird nicht gelöscht")
+            if request.form["del-title"] != "" and request.form["del-title"] != request.form["title"]: return page(
+                "TITEL NICHT KORREKT, Rezept wird nicht gelöscht")
             id = request.form["id"] if request.form.get("id", "") != "" else uuid.uuid4().__str__()
-            if 'image' in request.files and request.files['image'].mimetype in {'image/webp', 'image/jpeg', 'image/png'}:
+            if 'image' in request.files and request.files['image'].mimetype in {'image/webp', 'image/jpeg',
+                                                                                'image/png'}:
                 if not os.path.exists('static'): os.makedirs('static')
                 request.files['image'].save(os.path.join('static', id))
             if DB_DRIVER == "JSON":
-                with open('data.json', 'r+') as db_file:
-                    try: recipes = json.load(db_file)
-                    except Exception: recipes = {}
+                with open(DATAFILE, 'r+') as db_file:
+                    try:
+                        recipes = json.load(db_file)
+                    except Exception:
+                        recipes = {}
                     if request.form["del-title"] != "":
                         if id in recipes: del recipes[id]
                         if os.path.isfile(os.path.join('static', id)): os.remove(os.path.join('static', id))
                     else:
-                        recipes[id] = dict(title=request.form["title"][0:3000], ingredients=request.form["ingredients"][0:3000], prep=request.form["prep"][0:3000], tags=request.form["tags"][0:3000], cvss=0.0)
+                        recipes[id] = dict(title=request.form["title"][0:3000],
+                                           ingredients=request.form["ingredients"][0:3000],
+                                           prep=request.form["prep"][0:3000], tags=request.form["tags"][0:3000],
+                                           cvss=0.0)
                     db_file.seek(0)
                     db_file.truncate()
                     json.dump(recipes, db_file, indent=4)
@@ -157,42 +174,52 @@ def main():
                     conn.cursor().execute("DELETE FROM recipes WHERE id = ?", (id,))
                     if os.path.isfile(os.path.join('static', id)): os.remove(os.path.join('static', id))
                 else:
-                    conn.cursor().execute("INSERT OR REPLACE INTO recipes VALUES (?, ?, ?, ?, ?, ?)", (id, request.form["title"][0:3000], request.form["ingredients"][0:3000], request.form["prep"][0:3000], request.form["tags"][0:3000], request.form.get('cvss', 0.0)))
+                    conn.cursor().execute("INSERT OR REPLACE INTO recipes VALUES (?, ?, ?, ?, ?, ?)", (
+                    id, request.form["title"][0:3000], request.form["ingredients"][0:3000],
+                    request.form["prep"][0:3000], request.form["tags"][0:3000], request.form.get('cvss', 0.0)))
                 conn.commit()
             template = jinja2.Environment().from_string(page(edit_page))
-            if request.form["del-title"]==request.form["title"]: return make_response(redirect("/"))
-            return make_response(redirect("/r/"+id))
+            if request.form["del-title"] == request.form["title"]: return make_response(redirect("/"))
+            return make_response(redirect("/r/" + id))
         else:
             return page("FALSCHE PASSPHRASE, Rezept nicht angelegt / editiert / gelöscht")
     if DB_DRIVER == "JSON":
         recipes = get_json_db()
     elif DB_DRIVER == "SQLITE":
-        recipe_rows = get_sqlite_db().cursor().execute("SELECT title, ingredients, prep, tags, id, cvss FROM recipes ORDER BY title ASC").fetchall()
+        recipe_rows = get_sqlite_db().cursor().execute(
+            "SELECT title, ingredients, prep, tags, id, cvss FROM recipes ORDER BY title ASC").fetchall()
         recipes = [dict(row) for row in recipe_rows]
     template = jinja2.Environment().from_string(page(main_page))
     return template.render(recipes=recipes, recipes_count=len(recipes))
 
+
 @app.route("/e/<id>")
 def rezepte_edit(id):
-    if id!="new":
+    if id != "new":
         recipe = get_rezept(id)
         if recipe is None: return page("Rezept nicht gefunden :(")
     else:
-        recipe = dict(title="",tags="",prep="",ingredients="",id="")
+        recipe = dict(title="", tags="", prep="", ingredients="", id="")
     template = jinja2.Environment().from_string(page(edit_page))
-    return make_response( template.render(r=recipe, authenticated=('authenticated' in session), img_url=url_for('static', filename=recipe['id']), has_image=os.path.isfile(os.path.join('static', recipe['id']))))
+    return make_response(template.render(r=recipe, authenticated=('authenticated' in session),
+                                         img_url=url_for('static', filename=recipe['id']),
+                                         has_image=os.path.isfile(os.path.join('static', recipe['id']))))
+
 
 @app.route("/r/<id>")
 def rezepte_show(id):
     recipe = get_rezept(id)
     if recipe is None: return page("Rezept nicht gefunden :(")
     template = jinja2.Environment().from_string(page(recipe_page))
-    return template.render(r=recipe, img_url=url_for('static', filename=recipe['id']), has_image=os.path.isfile(os.path.join('static', recipe['id'])))
+    return template.render(r=recipe, img_url=url_for('static', filename=recipe['id']),
+                           has_image=os.path.isfile(os.path.join('static', recipe['id'])))
+
 
 def get_rezept(id):
     if DB_DRIVER == "JSON":
         recipe_row = list(filter(lambda r: r["id"] == id, get_json_db()))
         recipe_row = recipe_row[0] if recipe_row else None
     elif DB_DRIVER == "SQLITE":
-        recipe_row = get_sqlite_db().cursor().execute("SELECT title, ingredients, prep, tags, id, cvss FROM recipes WHERE id = ?", (id,)).fetchone()
+        recipe_row = get_sqlite_db().cursor().execute(
+            "SELECT title, ingredients, prep, tags, id, cvss FROM recipes WHERE id = ?", (id,)).fetchone()
     return dict(recipe_row) if recipe_row is not None else None
