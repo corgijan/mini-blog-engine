ab841ca28ae3ccef9539c23e1003c66dee536806
Iamjava <jan.vaorin@tu-dortmund.de>
2024-10-21 15:28:09 +0200

    default for driver set

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.secret_key = os.urandom(64)
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 
-DB_DRIVER = "JSON"  # JSON or SQLITE
+DB_DRIVER = os.environ.get("DRIVER") or "SQLITE"  # JSON or SQLITE
 DATAFILE = os.environ.get("DATA_PATH") or "data/data.json"
 PASSPHRASE = os.environ.get("RECIPE_PASSPHRASE") or "ichessegernekuchen"
 
