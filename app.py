578da50c6b6638b1b915b555c8a5267252b5b1c5
jule.jaenner <jule.jaenner@adesso.de>
2025-03-10 18:35:12 +0100

    favicon repair

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, make_response, request, session, g, url_for, render_template, send_from_directory
+from flask import Flask, redirect, make_response, request, session, g, url_for
 import jinja2, uuid, os, sqlite3, json
 import os
 
@@ -122,6 +122,6 @@ def get_rezept(id):
     return dict(recipe_row) if recipe_row is not None else None
 
 
-@app.route('/favicon.png')
+@app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.png', mimetype='image/x-icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/x-icon')
