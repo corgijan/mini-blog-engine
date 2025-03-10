7c6fc77baebd38c4d0aa378a76c3372d1af0cd6c
jule.jaenner <jule.jaenner@adesso.de>
2025-03-10 18:27:29 +0100

    favicon image added as png

--- app.py
+++ app.py
@@ -122,6 +122,6 @@ def get_rezept(id):
     return dict(recipe_row) if recipe_row is not None else None
 
 
-@app.route('/favicon.ico')
+@app.route('/favicon.png')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/x-icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.png', mimetype='image/x-icon')
